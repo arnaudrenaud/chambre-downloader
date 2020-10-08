628bbe28da426e80d7ef5e66a43fa1917aac2bef
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-10-08 18:27:03 +0200

    Search videos matching keyword and start date

--- download-videos.js
+++ download-videos.js
@@ -1,34 +1,56 @@
+require("dotenv").config();
+const fetch = require("node-fetch");
 const fs = require("fs");
 const path = require("path");
-const { getInfo } = require("ytdl-core");
 const ytdl = require("ytdl-core");
+const { downloadFromInfo } = require("ytdl-core");
 
 const DOWNLOAD_VIDEOS_DIRECTORY = "downloaded-videos";
 
-const getVideos = () => {
-  return ["HySUWruiU4A"];
+if (!fs.existsSync(DOWNLOAD_VIDEOS_DIRECTORY)) {
+  fs.mkdirSync(DOWNLOAD_VIDEOS_DIRECTORY);
+}
+
+const PATH_TO_WRITE_TO = path.join(
+  DOWNLOAD_VIDEOS_DIRECTORY,
+  new Date().toISOString().replace(/:/g, "-")
+);
+fs.mkdirSync(PATH_TO_WRITE_TO);
+
+const getVideoIds = async () => {
+  const searchForXLastHours = process.env.SEARCH_FOR_X_LAST_HOURS || 2;
+  const now = new Date();
+  const startDate = new Date(
+    now.setHours(now.getHours() - searchForXLastHours)
+  );
+  const searchKeyword = process.env.SEARCH_KEYWORD || "chambre";
+  const response = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?order=date&publishedAfter=${startDate.toISOString()}&q=${searchKeyword}&maxResults=50&key=${
+      process.env.YOUTUBE_DATA_API_KEY
+    }`,
+    {
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+  const searchResults = await response.json();
+  return searchResults.items.map((item) => item.id.videoId);
 };
 
-const downloadVideos = () => {
-  if (!fs.existsSync(DOWNLOAD_VIDEOS_DIRECTORY)) {
-    fs.mkdirSync(DOWNLOAD_VIDEOS_DIRECTORY);
-  }
-  const pathToWriteTo = path.join(
-    DOWNLOAD_VIDEOS_DIRECTORY,
-    new Date().toISOString().replace(/:/g, "-")
-  );
-  fs.mkdirSync(pathToWriteTo);
-  const videoIds = getVideos();
+const downloadVideos = (videoIds) => {
   videoIds.forEach(async (videoId) => {
     const videoUrl = `http://www.youtube.com/watch?v=${videoId}`;
-    console.log(`Downloading info for ${videoUrl}`);
-    const info = await getInfo(videoUrl);
-    // console.log(info)
     console.log(`Downloading video for ${videoUrl}`);
     ytdl(videoUrl).pipe(
-      fs.createWriteStream(path.join(pathToWriteTo, `${videoId}.mp4`))
+      fs.createWriteStream(path.join(PATH_TO_WRITE_TO, `${videoId}.mp4`))
     );
   });
 };
 
-downloadVideos();
+const downloadVideosAndInfo = async () => {
+  const videoIds = await getVideoIds();
+  downloadVideos(videoIds);
+};
+
+downloadVideosAndInfo();
